4b6edf76da73a2935657d35c93ebbe7a84acbc85
Dean Akinwale <dean.akinwale@hotmail.com>
2020-06-03 01:11:56 +0100

    Add project files.

---
+++ Sample.Shared/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sample.Shared
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Texture2D _sampleImage;
+        private SpriteFont _font;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //TODO: Uncomment line 32 to throw the exception
+            //_font = Content.Load<SpriteFont>("Fonts/SomeFont");
+            _sampleImage = Content.Load<Texture2D>("Images/Law");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            //spriteBatch.DrawString(_font, "Ahhhhhh its not working! :(", new Vector2(100, 100), Color.Black);
+            spriteBatch.Draw(_sampleImage, graphics.GraphicsDevice.Viewport.Bounds, Color.White);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
